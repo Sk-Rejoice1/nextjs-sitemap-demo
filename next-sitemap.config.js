f43ead75ce967cb979435e797c460b494335cb7c
Shardul <shardul123.rejoice@gmail.com>
2024-05-21 16:37:36 +0530

    chore: Format sitemap.xml and add additional paths
    This commit formats the sitemap.xml file to adhere to the XML standard and adds additional paths to the sitemap. The formatting is done using the Prettier XML plugin, and the additional paths are generated dynamically based on the products fetched from an external API. This improves the readability and SEO optimization of the sitemap.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -7,6 +7,36 @@ module.exports = {
     },
   ],
   generateRobotsTxt: true, // (optional)
-  // generateIndexSitemap: false, // (optional)
-  // ...other options
-}
+  generateIndexSitemap: false, // (optional)
+  transform: async (config, path) => {
+    // Use default transformation for all other cases
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: 'yearly',
+      priority: 0.7,
+      lastmod: new Date().toISOString(),
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
+  additionalPaths: async (config) => {
+    const result = [];
+    // const products = await getProducts();
+    const res = await fetch('https://dummyjson.com/products');
+    const data = await res.json();
+    const products = data?.products;
+
+    // static additional page
+    result.push({ loc: '/additional-page-1' });
+
+    // dynamic additional page
+    if (products?.length > 0) {
+      await products.forEach(async (product) => {
+        const productTitleModified = product.title.replace(/ /g, '-');
+        // using transformation from the current configuration
+        result.push(await config.transform(config, `/products/${productTitleModified}`));
+      });
+    }
+
+    return result;
+  },
+};
